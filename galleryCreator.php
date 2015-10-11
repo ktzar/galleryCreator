2304708ae555f12f6635e7f1a1c3fa60d3bb5c25
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-10-11 22:07:42 +0100

    Mustache template

--- galleryCreator.php
+++ galleryCreator.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
 date_default_timezone_set('Europe/London');
 
 function thumb($file, $thumbFile, $thumbWidth) {
@@ -22,8 +24,8 @@ function thumb($file, $thumbFile, $thumbWidth) {
 }
 
 function isDirOrDie($dir) {
-    if (!is_dir($source)) {
-        echo "$source is not a directory\n";
+    if (!is_dir($dir)) {
+        echo "$dir is not a directory\n";
         die;
     }
 }
@@ -35,19 +37,36 @@ isDirOrDie($source);
 isDirOrDie($target);
 
 $files = array_merge(glob("$source/*.JPG"), glob("$source/*.jpg"));
+mkdir("$target/thumbs");
+mkdir("$target/medium");
 
 $total = count($files);
-$output = array();
+$output = array(
+    'pics' => array()
+);
 $i = 0;
 foreach ($files as $file) {
     $data = exif_read_data($file);
-    $newname = strtotime($data['DateTime']) . ".jpg";
-    echo "$i / $total\tEncoding $file -> $newname\n";
+    if (isset($data['DateTime'])) {
+        $newname = strtotime($data['DateTime']);
+    } else {
+        $newname = str_pad($i, 4, '0', STR_PAD_LEFT);
+    }
+    $newname .= ".jpg";
+    echo $i ++ . " / $total\tEncoding $file -> $newname\n";
     thumb($file, "$target/thumbs/".$newname, 800);
     thumb($file, "$target/medium/".$newname, 1920);
-    $output[$newname] = $file;
-    $i++;
+    $output['pics'][] = array(
+        'thumb' => "./thumbs/$newname",
+        'medium' => "./medium/$newname",
+        'large' => "./full/" . basename($file),
+    );
 }
 
-echo "Writing references to output.json\n";
-file_put_contents("$target/output.json", json_encode($output, JSON_PRETTY_PRINT));
+$m = new Mustache_Engine;
+echo $m->render(
+    file_get_contents(__DIR__ . '/views/gallery.mustache'),
+    $output
+);
+//echo "Writing references to output.json\n";
+//file_put_contents("$target/output.json", json_encode($output, JSON_PRETTY_PRINT));
